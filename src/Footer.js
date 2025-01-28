c13f6ebaf8c95dc26d3b4e81767e84900f818a44
gannon-guess <gannon.guess@gmail.com>
2025-01-27 19:03:13 -0600

    Footer.js imports for css and link
    * footer formatted to bottom of page

--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,19 @@
-import "./components/Global.css"
-
-const discord_link = 'https://discord.com/invite/xc9MTRb9Hs'
+import './components/Footer.css';
+import { discord_link } from './constants';
 
 // here we define a Footer module that can be used in any of our pages
 function Footer() {
     return (
-        <footer className="bg-dark text-white text-center py-3">
-                
-            <p>2025 Collegiate Space Mining Competition</p>
-            <p>Cardinal Space Mining</p>
-            <p>
-                <a href={discord_link} target="_blank" rel="noopener noreferrer">
-                    <i className="cosmic-icon bi bi-discord" style={{ fontSize: '50px', marginLeft: '20px' }}></i>
-                </a>
-            </p>  
-        </footer>
+            <footer className="bg-dark text-white text-center py-3">
+                    
+                <p>2025 Collegiate Space Mining Competition</p>
+                <p>Cardinal Space Mining</p>
+                <p>
+                    <a href={discord_link} target="_blank" rel="noopener noreferrer">
+                        <i className="cosmic-icon bi bi-discord" style={{ fontSize: '50px', marginLeft: '20px' }}></i>
+                    </a>
+                </p>  
+            </footer>
     );
 }
 
