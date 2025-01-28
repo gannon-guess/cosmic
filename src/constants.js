e0e4a18221208a75d7c8c90c7334931d7e561ac7
gannon-guess <gannon.guess@gmail.com>
2025-01-27 19:01:43 -0600

    signup url added to constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 // link required for new members to join from site
 export const discord_link = 'https://discord.com/invite/xc9MTRb9Hs'
+export const signup_url = "https://forms.gle/o7JiHavvevEPctW96";
 
 // function to import to change the current page view
 export function changeView(viewer, setViewer, newView) {
