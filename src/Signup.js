f8471b61ef0d60e4f43c2a3b8a7aa159b71cd8ae
gannon-guess <gannon.guess@gmail.com>
2025-02-12 19:17:47 -0600

    signup page remove embedded form
    * remove embedded form from signup page, replace with a link to the external form in a new tab

--- src/Signup.js
+++ src/Signup.js
@@ -1,59 +1,28 @@
 import React from "react";
-import { Button } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import View from "./Views.js";
+import { signup_url } from "./constants.js";
 
-import { changeView } from "./constants.js";
-
-// This defines the google doc to be embedded for the user to fill out
-const signup_url = "https://forms.gle/o7JiHavvevEPctW96";
-
-function Signup({ viewer, setViewer }) {
-
+function Signup() {
     return (
-        <div> 
-            <div className="d-flex justify-content-center align-items-center min-vh-100 bg-light">
-                <div className="container p-5 bg-white shadow-lg rounded">
-                    <h1 className="text-center mb-4 text-primary">CoSMiC Signup</h1>
+        <div className=" text-center py-5">
+            <h2 className="mb-4">Sign Up for CoSMiC</h2>
+            <Button 
+                href={signup_url} 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                variant="primary" 
+                size="lg" 
+                className="px-4 py-2"
+            >
+                <i className="bi bi-pencil-square me-2"></i> Open Google Signup Form
+            </Button>
+            <>
+            </>
 
-                    <a
-                        href={signup_url}
-                        target="_blank" // Opens link in a new tab
-                        rel="noopener noreferrer" // Prevents security risks
-                        >
-                        Signup Form
-                    </a>
-
-                    {/* DOES NOT WORK BECAUSE OUR FORM REQUIRES FILE UPLOAD
-                        LEAVING IN CASE WORK AROUND IS FOUND
-                    */}
-                    {/* Google Form Embedded in an iframe */}
-                    {/* <div className="embed-responsive embed-responsive-16by9 mb-4">
-                        <iframe 
-                            src={signup_url} 
-                            width="100%" 
-                            height="600" 
-                            frameBorder="0" 
-                            marginHeight="0" 
-                            marginWidth="0"
-                            title="CoSMiC Signup Form"
-                            className="embed-responsive-item rounded"
-                        >
-                            Loading…
-                        </iframe>
-                    </div> */}
-
-                    {/* Back to Main Page Button */}
-                    <div className="text-center">
-                        <Button onClick={() => changeView(viewer, setViewer, View.MAIN)} variant="primary" className="mt-4">
-                            <i className="bi bi-arrow-left-circle me-2"></i>Back to Main Page
-                        </Button>
-                    </div>
-                </div>
-            </div>
         </div>
     );
 }
