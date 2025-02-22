e630cd3610b21e881450e894c401bd6378bb5cb5
gannon-guess <gannon.guess@gmail.com>
2025-02-22 14:35:59 -0600

    App.js routes to tourism

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import View from "./Views.js"
+import View from "./Views.js";
 import NavBar from "./NavBar.js";
-import Main from "./Main.js"
-import FAQ from "./FAQ.js"
-import Footer from "./Footer.js"
-import Signup from "./Signup.js"
+import Main from "./Main.js";
+import FAQ from "./FAQ.js";
+import Footer from "./Footer.js";
+import Signup from "./Signup.js";
+import Tourism from "./Tourism.js";
 
 import "./components/Global.css";
 
-function App()
-{
-
-    // we display the page based on viewer for module, single page development
-    return(
-        <BrowserRouter>
-            <NavBar />
-            <main>
-                <Routes>
-                    <Route index element={<Main />} />
-                    <Route path="signup" element={<Signup/>} />
-                    <Route path="faq" element={<FAQ />} />
-                </Routes>
-            </main>
-            <Footer/>
-        </BrowserRouter>
-        
+function App() {
+    return (
+        <div className="app-container">
+            <BrowserRouter>
+                <NavBar />
+                <main className="main-content">
+                    <Routes>
+                        <Route index element={<Main />} />
+                        <Route path="signup" element={<Signup />} />
+                        <Route path="faq" element={<FAQ />} />
+                        <Route path="tourism" element={<Tourism/>} />
+                    </Routes>
+                </main>
+            </BrowserRouter>
+            <Footer />
+        </div>
     );
 }
 
